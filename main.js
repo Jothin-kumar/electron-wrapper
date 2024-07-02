b19d897891ae0ee170b9b1e0dcf5c31924a49ae1
Jothin-Kumar <contact@jothin.tech>
2024-07-02 19:57:52 +0530

    basic functionality

--- main.js
+++ main.js
@@ -1,25 +1,50 @@
-const { app, BrowserWindow, screen } = require('electron')
+const { app, BrowserWindow, screen, ipcMain } = require('electron')
+const nodePath = require("path");
 
-const createBrowserWindow = (width, height, url) => {
+const createBrowserWindow = (mainWindow, width, height, url) => {
     const win = new BrowserWindow({
         width: width,
         height: height,
         autoHideMenuBar: true,
+        minHeight: height / 2,
+        minWidth: width / 2
     })
 
     win.loadURL(url, { userAgent: 'Mozilla/5.0 (X11; Linux i686; rv:127.0) Gecko/20100101 Firefox/127.0' })
+    win.on('closed', () => {
+        mainWindow.show()
+    })
 }
 
+PrimaryScreen = {}
+
 app.whenReady().then(() => {
-    const { screenWidth, screenHeight } = screen.getPrimaryDisplay().workAreaSize
-    if (screenWidth < 1000 || screenHeight < 690) {
-        app.quit()
-    }
+    PrimaryScreen.Resolution = screen.getPrimaryDisplay().workAreaSize
     const mainWindow = new BrowserWindow({
         width: 800,
         height: 500,
         frame: false,
-        resizable: false
+        resizable: false,
+        webPreferences: {
+            preload: nodePath.join(__dirname, 'preload.js'),
+            nodeIntegration: false,
+            contextIsolation: true,
+            devTools: false
+        }
     })
     mainWindow.loadFile('main.html')
+    ipcMain.on('open-app', (evt, arg) => {
+        mainWindow.hide()
+        switch (arg) {
+            case 'WhatsApp':
+                createBrowserWindow(mainWindow, PrimaryScreen.Resolution.width, PrimaryScreen.Resolution.height, 'https://web.whatsapp.com')
+                break
+            case 'InstaGram':
+                createBrowserWindow(mainWindow, PrimaryScreen.Resolution.width, PrimaryScreen.Resolution.height, 'https://instagram.com')
+                break
+            case 'SnapChat':
+                createBrowserWindow(mainWindow, PrimaryScreen.Resolution.width, PrimaryScreen.Resolution.height, 'https://web.snapchat.com/')
+                break
+        }
+    })
 })
